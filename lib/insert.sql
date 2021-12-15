c5907411f589a76e488fa5d4ed03a890e4102925
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-12-15 15:30:16 -0600

    commit tree 7bb31b1a42df66eececf6ebb00292f754215d063

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("tim", 4, "M", "blue", "angry", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("tony", 42, "M", "green", "sad", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("tania", 5, "F", "red", "calm", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("tinkles", 12, "F", "yellow", "angry", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("tiny tim", 12, "M", "yellow", "angry", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("tickles", 12, "F", "yellow", "angry", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("toby", 12, "M", "yellow", "angry", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(null, 12, "F", "yellow", "angry", true);
